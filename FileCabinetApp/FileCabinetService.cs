8e5862bd818c75c29c645885201239694599978c
alesya.zhovnerik.06@tutanota.com <alesya.zhovnerik.06@tutanota.com>
2021-01-27 15:29:11 +0300

    Add FileCabinetService.cs file

---
+++ FileCabinetApp/FileCabinetService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetService
+    {
+        private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
+
+        public int CreateRecord(string FirstName, string Lastname, DateTime dateOfBirth)
+        {
+            return 0;
+        }
+
+        public FileCabinetRecord[] GetRecords()
+        {
+            return new FileCabinetRecord[] { };
+        }
+
+        public int GetStat()
+        {
+            return 0;
+        }
+    }
+}
