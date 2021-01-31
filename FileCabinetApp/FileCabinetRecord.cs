756e4fb9a717ca6a0521d5ea98494922f49e0d97
alesya.zhovnerik.06@tutanota.com <alesya.zhovnerik.06@tutanota.com>
2021-01-31 11:36:09 +0300

    Add guard clauses to CreateRecord method.

--- FileCabinetApp/FileCabinetRecord.cs
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -6,18 +6,133 @@ namespace FileCabinetApp
 {
     public class FileCabinetRecord
     {
+        private string firstname;
+        private string lastName;
+        private DateTime dateOfbirth;
+        private decimal salary;
+        private char key;
+        private short passForCabinet;
+
         public int Id { get; set; }
 
-        public string FirstName { get; set; }
+        public string FirstName
+        {
+            get
+            {
+                return this.firstname;
+            }
 
-        public string LastName { get; set; }
+            set
+            {
+                if (value is null)
+                {
+                    throw new ArgumentNullException(nameof(value), $"{nameof(value)} is null.");
+                }
 
-        public DateTime DateOfBirth { get; set; }
+                if (!((value.Length >= 2 && value.Length <= 60) || string.IsNullOrWhiteSpace(value)))
+                {
+                    throw new ArgumentException($"Incorrect data entry.", nameof(value));
+                }
 
-        public decimal Salary { get; set; }
+                this.firstname = value;
+            }
+        }
 
-        public char Key { get; set; }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
 
-        public short PassForCabinet { get; set; }
+            set
+            {
+                if (value is null)
+                {
+                    throw new ArgumentNullException(nameof(value), $"{nameof(value)} is null.");
+                }
+
+                if (!((value.Length >= 2 && value.Length <= 60) || string.IsNullOrWhiteSpace(value)))
+                {
+                    throw new ArgumentException($"Incorrect data entry.", nameof(value));
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get
+            {
+                return this.dateOfbirth;
+            }
+
+            set
+            {
+               DateTime date = new DateTime(1950, 1, 1);
+               if (value > DateTime.Now || value < date)
+               {
+                  throw new ArgumentException("Incorrect data entry.", nameof(value));
+               }
+
+               this.dateOfbirth = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+
+            set
+            {
+                if (value > 10000 || value < 500)
+                {
+                    throw new ArgumentException("Incorrect data entry.", nameof(value));
+                }
+
+                this.salary = value;
+            }
+        }
+
+
+        public char Key
+        {
+            get
+            {
+                return this.key;
+            }
+
+            set
+            {
+                if (value < 'A' || value > 'Z')
+                {
+                    throw new ArgumentException("Incorrect data entry.", nameof(value));
+                }
+
+                this.key = value;
+            }
+        }
+
+        public short PassForCabinet
+        {
+            get
+            {
+                return this.passForCabinet;
+            }
+
+            set
+            {
+                if (value < 1 || value > 99)
+                {
+                    throw new ArgumentException("Incorrect data entry.", nameof(value));
+                }
+
+                this.passForCabinet = value;
+            }
+        }
     }
 }
